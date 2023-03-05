7bf139724954d2ffe872c6c7066d3af8818e42e9
Matthew Benkart <Mthwate@users.noreply.github.com>
2023-03-04 16:12:05 -0800

    Update install-nix.sh
    Fix a few bugs in the install script

--- install-nix.sh
+++ install-nix.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
 CONFIG=$1
-DISK=$1
+DISK=$2
+
+if [ "$EUID" -ne 0 ]
+then
+  echo "Please run as root"
+  exit
+fi
 
 parted $DISK -- mklabel gpt
 parted $DISK -- mkpart primary 512MB 100%
 parted $DISK -- mkpart ESP fat32 1MB 512MB
 parted $DISK -- set 2 esp on
 
-mkfs.btrfs $DISK1
-mkfs.fat -F 32 -n boot $DISK2
+mkfs.btrfs ${DISK}1
+mkfs.fat -F 32 -n boot ${DISK}2
 
-mount $DISK1 /mnt
+mount ${DISK}1 /mnt
 mkdir -p /mnt/boot
-mount $DISK2 /mnt/boot
+mount ${DISK}2 /mnt/boot
 
 nixos-generate-config --root /mnt
 
 cp $CONFIG /mnt/etc/nixos/configuration.nix
+
+nixos-install
